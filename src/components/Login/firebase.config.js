fa86ac41b78254fae07aaa3fea5b053b4316f3c4
Meherameem <54639359+Meherameem@users.noreply.github.com>
2020-10-04 02:02:27 +0600

    client side of burj-al-arab site

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCCRI3b9OclqNO7otLtSBwqVieBz2FPBeU",
+  authDomain: "burj-al-arab-a2566.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-a2566.firebaseio.com",
+  projectId: "burj-al-arab-a2566",
+  storageBucket: "burj-al-arab-a2566.appspot.com",
+  messagingSenderId: "473212216231",
+  appId: "1:473212216231:web:e8b8be9890cf83bc9acc82"
+};
+  export default firebaseConfig;
+
+  
